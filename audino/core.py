20d5335ddb3113f8b2c683588e462589a337490a
Josh <8677174+bijij@users.noreply.github.com>
2024-10-04 00:04:49 +1000

    Update deps

--- audino/core.py
+++ audino/core.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 import asyncio
+import logging
+import os
 from collections.abc import Callable, Coroutine
-from typing import Any
+from typing import Any, overload
 
 from rayquaza import Mediator, Message
 
 __all__ = ("HealthTracker",)
 
+
+_log = logging.getLogger(__name__)
 
 class _HealthStatus(Message):
     __slots__: tuple[str, ...] = ("type", "healthy")
@@ -20,12 +24,30 @@ class _HealthStatus(Message):
 class HealthTracker:
     """
     The HealthTracker class is used to track the health status of components.
+
+    Parameters
+    ----------
+    mediator: Optional[:class:`Mediator`]
+        The mediator to use for communication between components, if not provided an internal mediator will be used.
+    mediator_channel: Optional[:class:`str`]
+        The mediator channel to use for communication between components, if not provided an internal channel will be used.
     """
 
-    def __init__(self) -> None:
+    @overload
+    def __init__(self, mediator: None = ...) -> None:
+        ...
+
+    @overload
+    def __init__(self, *, mediator: Mediator = ..., mediator_channel: str | None = ...) -> None:
+        ...
+
+    def __init__(self, mediator: Mediator | None = None, *, mediator_channel: str | None = None) -> None:
         self._heath_states: dict[str, bool] = {}
-        self._mediator: Mediator = Mediator()
+        self._mediator: Mediator = mediator or Mediator()
+        self._mediator_channel: str = mediator_channel or os.urandom(16).hex()
         self._lock: asyncio.Lock = asyncio.Lock()
+
+        _log.debug("Initailsed health tracker with mediator %s on channel %s", self._mediator, self._mediator_channel)
 
     async def get_health(self, type: str) -> bool:
         """|coro|
@@ -60,8 +82,10 @@ class HealthTracker:
         async with self._lock:
             self._heath_states[type] = healthy
 
+        _log.debug("Health status of %s set to %s", type, healthy)
+
         message = _HealthStatus(type, healthy)
-        await self._mediator.publish(message, wait=False)
+        await self._mediator.publish(self._mediator_channel, message, wait=False)
 
     def subscribe(self, callback: Callable[[str, bool], Coroutine[Any, Any, None]]) -> None:
         """Subscribe to health status changes.
@@ -76,4 +100,4 @@ class HealthTracker:
         async def _callback(message: _HealthStatus) -> None:
             await callback(message.type, message.healthy)
 
-        self._mediator.create_subscription(_HealthStatus, _callback)
+        self._mediator.create_subscription(self._mediator_channel, _HealthStatus, _callback)
